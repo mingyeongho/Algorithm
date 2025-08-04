2bfb93e01d955fcb64b4bd1e1c60f38cb4404de1
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2025-08-04 17:16:10 +0900

    [Silver V] Title: 방 번호, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+n = input().rstrip()
+
+
+def fn(n):
+  cnts = [0] * 10
+  for i in n:
+    if i == '6' or i == '9':
+      if cnts[6] <= cnts[9]:
+        cnts[6] += 1
+      else:
+        cnts[9] += 1
+    else:
+      cnts[int(i)] += 1
+  return max(cnts)
+
+
+print(fn(n))

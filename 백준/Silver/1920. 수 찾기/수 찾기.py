3f5057e3ecdaf6df0c687394a1998777579cd9dd
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2025-08-18 11:02:16 +0900

    [Silver IV] Title: 수 찾기, Time: 156 ms, Memory: 52416 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+targets = set(map(int, input().split()))
+m = int(input().rstrip())
+datas = list(map(int, input().split()))
+
+for data in datas:
+    print(1 if data in targets else 0)

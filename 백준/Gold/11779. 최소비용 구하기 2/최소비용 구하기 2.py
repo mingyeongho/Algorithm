72afb555fb065793d443830e3eab33e6163747ae
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2025-09-16 09:07:01 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 296 ms, Memory: 58384 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -0,0 +1,49 @@
+"""
+A번째 도시에서 B번째 도시까지 가는 버스 비용을 최소화
+다익스크라 알고리즘 사용
+"""
+
+import sys
+from collections import defaultdict
+import heapq
+input = sys.stdin.readline
+
+n = int(input().strip())
+m = int(input().strip())
+tree = defaultdict(list)
+for _ in range(m):
+    s, e, c = map(int, input().split())
+    tree[s].append((c, e))
+S, E = map(int, input().split())
+
+
+def dijkstra(start):
+    dist = [1e9] * (n+1)
+    dist[start] = 0
+    hq = [(0, start)]
+    path = [-1] * (n+1)
+
+    while hq:
+        cost, node = heapq.heappop(hq)
+        if cost > dist[node]:
+            continue
+
+        for nxt_cost, nxt_node in tree[node]:
+            new_cost = cost + nxt_cost
+            if new_cost < dist[nxt_node]:
+                dist[nxt_node] = new_cost
+                heapq.heappush(hq, (new_cost, nxt_node))
+                path[nxt_node] = node
+    return dist, path
+
+
+d, p = dijkstra(S)
+print(d[E])
+path = [E]
+while p[E] != S:
+    path.append(p[E])
+    E = p[E]
+path.append(S)
+path.reverse()
+print(len(path))
+print(*path)

e492d3f95f236803d22dc66797804b2a2a75f389
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2025-09-23 12:36:32 +0900

    [Gold V] Title: 용액, Time: 112 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -4,24 +4,22 @@ input = sys.stdin.readline
 N = int(input().strip())
 S = list(map(int, input().split()))
 
-l, r = 0, N - 1
-best_abs = float('inf')
-ans_l, ans_r = S[l], S[r]
+left, right = 0, N-1
+mn = 2_000_000_001
+ans_left, ans_right = S[left], S[right]
 
-while l < r:
-    s = S[l] + S[r]
+while left < right:
+    s = S[left] + S[right]
     a = abs(s)
 
-    if a < best_abs:
-        best_abs = a
-        ans_l, ans_r = S[l], S[r]
-        if best_abs == 0:
+    if a < mn:
+        mn = a
+        ans_left, ans_right = S[left], S[right]
+        if mn == 0:
             break
-
-    # 합이 0보다 작으면 값(합)을 키워야 하므로 l를 오른쪽으로 이동
     if s < 0:
-        l += 1
+        left += 1
     else:
-        r -= 1
+        right -= 1
 
-print(ans_l, ans_r)
+print(ans_left, ans_right)

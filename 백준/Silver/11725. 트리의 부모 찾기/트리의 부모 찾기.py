97a0ee7a304dcf4773295937e596653142ebc786
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2025-09-26 03:23:31 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 316 ms, Memory: 51560 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -3,24 +3,28 @@ from collections import deque
 input = sys.stdin.readline
 
 N = int(input().strip())
-
-graph = [[] for _ in range(N+1)]
-
+tree = [[] for _ in range(N+1)]
 for _ in range(N-1):
-    i, j = map(int, input().split())
-    graph[i].append(j)
-    graph[j].append(i)
+    A, B = map(int, input().split())
+    tree[A].append(B)
+    tree[B].append(A)
 
-visited = [True, True] + [False] * (N-1)
+
 parents = [0, 1] + [-1] * (N-1)
 
-queue = deque([1])
-visited[1] = True
-while queue:
-    start = queue.popleft()
-    for end in graph[start]:
-        if not visited[end]:
-            parents[end] = start
-            visited[end] = True
-            queue.append(end)
-print(*parents[2:], sep='\n')
+
+def bfs(node):
+    queue = deque([node])
+    while queue:
+        xp = queue.popleft()
+        for nx in tree[xp]:
+            if parents[xp] == nx:
+                continue
+            queue.append(nx)
+            parents[nx] = xp
+
+
+bfs(1)
+
+for i in range(2, N+1):
+    print(parents[i])

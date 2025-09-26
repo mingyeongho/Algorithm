8ce994a1d390edfda18242dc7fed6578fbdb1871
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2025-09-26 12:58:59 +0900

    [Gold IV] Title: RGB거리 2, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.py
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.py
@@ -2,28 +2,28 @@ import sys
 input = sys.stdin.readline
 
 N = int(input().strip())
-cost = [list(map(int, input().split())) for _ in range(N)]
+costs = [list(map(int, input().split())) for _ in range(N)]
 
-INF = 1_000 * 1_000
-# dp[i][c][s]: i번째 집까지 칠했을 때, i번 집 색=c, 첫 집 색=s인 최소 비용
-dp = [[[INF] * 3 for _ in range(3)] for _ in range(N)]
+INF = 1_000 * 1_000 + 1
+# dp[k][i][j]: 첫 번째 집 색=k, i번째 집 색=j 일 때 최소비용
+dp = [[[INF] * 3 for _ in range(N)] for _ in range(3)]
 
-# 시작(0번째 집) 초기화: 첫 집 색 s만 유효
-for s in range(3):
-    dp[0][s][s] = cost[0][s]
+# 첫 집 초기화
+for k in range(3):
+    dp[k][0][k] = costs[0][k]
 
 # 점화식
 for i in range(1, N):
-    for s in range(3):
-        dp[i][0][s] = cost[i][0] + min(dp[i-1][1][s], dp[i-1][2][s])
-        dp[i][1][s] = cost[i][1] + min(dp[i-1][0][s], dp[i-1][2][s])
-        dp[i][2][s] = cost[i][2] + min(dp[i-1][0][s], dp[i-1][1][s])
+    for k in range(3):  # 시작 색
+        dp[k][i][0] = costs[i][0] + min(dp[k][i-1][1], dp[k][i-1][2])
+        dp[k][i][1] = costs[i][1] + min(dp[k][i-1][0], dp[k][i-1][2])
+        dp[k][i][2] = costs[i][2] + min(dp[k][i-1][0], dp[k][i-1][1])
 
-# 원형 제약: 마지막 집 색 != 첫 집 색
+# 원형 제약: 첫 집 색 != 마지막 집 색
 ans = INF
-for s in range(3):
-    for c in range(3):
-        if c != s:
-            ans = min(ans, dp[N-1][c][s])
+for k in range(3):
+    for j in range(3):
+        if k != j:
+            ans = min(ans, dp[k][N-1][j])
 
 print(ans)

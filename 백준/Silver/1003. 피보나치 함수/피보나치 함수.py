bba3819f7a740d874b9bdae61f72577fcc85f1c7
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2025-09-28 13:38:01 +0900

    [Silver III] Title: 피보나치 함수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,19 +1,13 @@
 import sys
-
 input = sys.stdin.readline
 
-n = int(input().rstrip())
+T = int(input().strip())
 
-for _ in range(n):
-  m = int(input().rstrip())
+dp = [(1, 0), (0, 1)]
+for i in range(2, 41):
+    dp.append((dp[i-2][0] + dp[i-1][0], dp[i-2][1] + dp[i-1][1]))
 
-  zero = [0] * 41
-  one = [0] * 41
 
-  zero[0], zero[1] = 1, 0
-  one[0], one[1] = 0, 1
-
-  for i in range(2, m + 1):
-    zero[i] = zero[i - 2] + zero[i - 1]
-    one[i] = one[i - 2] + one[i - 1]
-  print(zero[m], one[m])
+for _ in range(T):
+    N = int(input().strip())
+    print(*dp[N])

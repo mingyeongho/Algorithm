e7850f6470a98111840c4882f66785bc6b53fee5
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2025-10-04 19:57:50 +0900

    [Silver I] Title: 스티커, Time: 624 ms, Memory: 47872 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -2,16 +2,15 @@ import sys
 input = sys.stdin.readline
 
 T = int(input().strip())
-
 for _ in range(T):
-    n = int(input().strip())
-    graph = [list(map(int, input().split())) for _ in range(2)]
+    N = int(input().strip())
+    A = [list(map(int, input().split())) for _ in range(2)]
 
-    dp = [[-1] * n for _ in range(2)]
-    dp[0][0] = graph[0][0]
-    dp[1][0] = graph[1][0]
+    dp = [[-1] * N for _ in range(2)]
+    dp[0][0] = A[0][0]
+    dp[1][0] = A[1][0]
 
-    for i in range(1, n):
-        dp[0][i] = max(dp[0][i-1], dp[1][i-1] + graph[0][i])
-        dp[1][i] = max(dp[1][i-1], dp[0][i-1] + graph[1][i])
-    print(max(dp[0][n-1], dp[1][n-1]))
+    for j in range(1, N):
+        dp[0][j] = max(dp[0][j-1], dp[1][j-1] + A[0][j])
+        dp[1][j] = max(dp[1][j-1], dp[0][j-1] + A[1][j])
+    print(max(dp[0][N-1], dp[1][N-1]))

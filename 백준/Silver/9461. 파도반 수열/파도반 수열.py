2c53499f4b78350cbf90e50cf4c780e06a1cc0d5
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2025-10-04 11:00:58 +0900

    [Silver III] Title: 파도반 수열, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+dp = [0, 1, 1, 1, 2, 2]
+
+for i in range(6, 101):
+  dp.append(dp[i-1] + dp[i-5])
+
+for _ in range(n):
+  m = int(input().rstrip())
+  print(dp[m])

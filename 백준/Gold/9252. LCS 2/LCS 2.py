ad49c9695bada4eb0b2afa2bb5f47cf7416f5e26
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2025-10-07 22:15:08 +0900

    [Gold IV] Title: LCS 2, Time: 428 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -3,16 +3,28 @@ input = sys.stdin.readline
 
 A = input().strip()
 B = input().strip()
+n = len(A)
+m = len(B)
 
-dp = [[""] * (len(B)+1) for _ in range(len(A)+1)]
-for i in range(1, len(A)+1):
-    for j in range(1, len(B)+1):
+dp = [[0] * (m+1) for _ in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, m+1):
         if A[i-1] == B[j-1]:
-            dp[i][j] = dp[i-1][j-1] + A[i-1]
+            dp[i][j] = dp[i-1][j-1] + 1
         else:
-            if len(dp[i-1][j]) < len(dp[i][j-1]):
-                dp[i][j] = dp[i][j-1]
-            else:
-                dp[i][j] = dp[i-1][j]
-print(len(dp[-1][-1]))
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+i, j = n, m
+lcs = []
+while i > 0 and j > 0:
+    if A[i-1] == B[j-1]:
+        lcs.append(A[i-1])
+        i -= 1
+        j -= 1
+    elif dp[i-1][j] >= dp[i][j-1]:
+        i -= 1
+    else:
+        j -= 1
 print(dp[-1][-1])
+lcs.reverse()
+print(''.join(lcs))

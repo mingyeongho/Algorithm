da09b383f5970192ddf186dbe744e1fcc004daa8
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2025-10-11 21:18:30 +0900

    [Gold V] Title: 치킨 배달, Time: 220 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -2,42 +2,40 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
+graph = [[0] * (N+1)] + [[0] + list(map(int, input().split()))
+                         for _ in range(N)]
 
-# 집과 치킨집 좌표 저장
-houses = []
-chickens = []
-for i in range(N):
-    for j in range(N):
-        if graph[i][j] == 1:
-            houses.append((i, j))
-        elif graph[i][j] == 2:
-            chickens.append((i, j))
+H = []
+C = []
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if graph[i][j] == 2:
+            C.append((i, j))
+        elif graph[i][j] == 1:
+            H.append((i, j))
 
-# 치킨집 선택
-selected = [-1] * M
-answer = float("Inf")
+answer = []
+dist = float("Inf")
 
 
 def backtrack(start, k):
-    global answer
+    global dist
 
     if k == M:
-        # 치킨집 선택 완료
-        total = 0
-        for hx, hy in houses:
-            dist = float("Inf")
-            for idx in selected:
-                cx, cy = chickens[idx]
-                dist = min(dist, abs(hx-cx) + abs(hy-cy))
-            total += dist
-        answer = min(answer, total)
+        d = 0
+        for hx, hy in H:
+            hd = float("Inf")
+            for cx, cy in answer:
+                cd = abs(hx - cx) + abs(hy - cy)
+                hd = min(hd, cd)
+            d += hd
+        dist = min(dist, d)
         return
-    for i in range(start, len(chickens)):
-        selected[k] = i
+    for i in range(start, len(C)):
+        answer.append(C[i])
         backtrack(i+1, k+1)
+        answer.pop()
 
 
 backtrack(0, 0)
-
-print(answer)
+print(dist)

3b28c9f2cfa3d7855ac63ffdddc5812f3eae5d4b
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2025-10-19 10:51:20 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 2220 ms, Memory: 35864 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -2,28 +2,26 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 
-S = []
+
+def post(start, end):
+    if start > end:
+        return
+    mid = end + 1
+    for i in range(start+1, end+1):
+        if pre[start] < pre[i]:
+            mid = i
+            break
+    post(start+1, mid-1)
+    post(mid, end)
+    print(pre[start])
+
+
+pre = []
 while True:
     try:
         n = int(input().strip())
-        S.append(n)
+        pre.append(n)
     except:
         break
 
-
-def recur(start, end):
-    if start > end:
-        return
-
-    mid = end+1
-    for i in range(start+1, end+1):
-        if S[start] < S[i]:
-            mid = i
-            break
-    recur(start+1, mid-1)
-    recur(mid, end)
-    print(S[start])
-    pass
-
-
-recur(0, len(S)-1)
+post(0, len(pre)-1)

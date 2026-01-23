9726713f6be339b224873e352ecb6ad6ff749d91
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2026-01-23 18:14:07 +0900

    [Gold III] Title: 불!, Time: 1612 ms, Memory: 145196 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -2,50 +2,45 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-r, c = map(int, input().split())
-graph = [list(input().strip()) for _ in range(r)]
+R, C = map(int, input().split())
+graph = [list(input().strip()) for _ in range(R)]
 
-direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+direction = [(0, 1), (0, -1), (-1, 0), (1, 0)]
 
-# 지훈이와 불이 빈 공간에 몇 분 후 도착하는지를 담을 이차원 배열 필요
-f_dist = [[-1] * c for _ in range(r)]
-j_dist = [[-1] * c for _ in range(r)]
+def func(R, C, graph) -> str | int:
+    dist = [[[-1, -1] for _ in range(C)] for _ in range(R)]
+    ji = deque()
+    fire = deque()
+    for i in range(R):
+        for j in range(C):
+            if graph[i][j] == "F":
+                fire.append((i, j))
+                dist[i][j][1] = 0
+            elif graph[i][j] == "J":
+                ji.append((i, j))
+                dist[i][j][0] = 0
+    
+    while fire:
+        xp, yp = fire.popleft()
+        for dx, dy in direction:
+            nx, ny = xp + dx, yp + dy
+            if not (0 <= nx < R and 0 <= ny < C):
+                continue
+            if dist[nx][ny][1] == -1 and graph[nx][ny] == ".":
+                dist[nx][ny][1] = dist[xp][yp][1] + 1
+                fire.append((nx, ny))
+                
+    while ji:
+        xp, yp = ji.popleft()
+        for dx, dy in direction:
+            nx, ny = xp + dx, yp + dy
+            if not (0 <= nx < R and 0 <= ny < C):
+                # 탈출
+                return dist[xp][yp][0] + 1
+            if dist[nx][ny][0] == -1 and graph[nx][ny] == "." and (dist[nx][ny][1] == -1 or dist[nx][ny][1] > dist[xp][yp][0] + 1):
+                dist[nx][ny][0] = dist[xp][yp][0] + 1
+                ji.append((nx, ny))
+    
+    return "IMPOSSIBLE"
 
-# 지훈이와 불의 위치를 큐에 저장
-j_queue = deque([])
-f_queue = deque([])
-for i in range(r):
-    for j in range(c):
-        if graph[i][j] == 'J':
-            j_queue.append((i, j))
-            j_dist[i][j] = 0
-        elif graph[i][j] == 'F':
-            f_queue.append((i, j))
-            f_dist[i][j] = 0
-
-while f_queue:
-    xp, yp = f_queue.popleft()
-    for dx, dy in direction:
-        nx, ny = xp + dx, yp + dy
-        if not (0 <= nx < r and 0 <= ny < c):
-            continue
-        if graph[nx][ny] != '#' and f_dist[nx][ny] == -1:
-            f_queue.append((nx, ny))
-            f_dist[nx][ny] = f_dist[xp][yp] + 1
-
-while j_queue:
-    xp, yp = j_queue.popleft()
-    for dx, dy in direction:
-        nx, ny = xp + dx, yp + dy
-        # 탈출 성공
-        if not (0 <= nx < r and 0 <= ny < c):
-            print(j_dist[xp][yp] + 1)
-            exit(0)
-        if graph[nx][ny] == '#' or j_dist[nx][ny] != -1:
-            continue
-        if f_dist[nx][ny] != -1 and f_dist[nx][ny] <= j_dist[xp][yp] + 1:
-            continue
-        j_queue.append((nx, ny))
-        j_dist[nx][ny] = j_dist[xp][yp] + 1
-
-print("IMPOSSIBLE")
+print(func(R, C, graph))

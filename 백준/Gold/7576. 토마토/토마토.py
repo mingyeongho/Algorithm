8f12f8a0f57cfbfeebab2e0a7e4f81674fc5c3b6
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2026-01-23 17:46:16 +0900

    [Gold V] Title: 토마토, Time: 1304 ms, Memory: 109092 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -2,33 +2,38 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-m, n = map(int, input().split())  # m: 가로, n: 세로
-graph = [list(map(int, input().split())) for _ in range(n)]
-direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-mx = 0
-queue = deque([])
+direction = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
+N, M = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(M)]
 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
-            queue.append((i, j))
-
-# bfs
-while queue:
-    xp, yp = queue.popleft()
-    for dx, dy in direction:
-        nx, ny = xp + dx, yp + dy
-        if not (0 <= nx < n and 0 <= ny < m):
-            continue
-        if graph[nx][ny] == 0:
-            queue.append((nx, ny))
-            graph[nx][ny] = graph[xp][yp] + 1
-
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 0:
-            print(-1)
-            exit(0)
-        mx = max(mx, graph[i][j])
-print(mx - 1)
+def func(N, M, graph) -> int:
+    dist = [[-1 for _ in range(N)] for _ in range(M)]
+    
+    queue = deque()
+    
+    for i in range(M):
+        for j in range(N):
+            if graph[i][j] == 1:
+                queue.append((i, j))
+                dist[i][j] = 0
+    
+    while queue:
+        xp, yp = queue.popleft()
+        for dx, dy in direction:
+            nx, ny = xp + dx, yp + dy
+            if not (0 <= nx < M and 0 <= ny < N):
+                continue
+            if graph[nx][ny] == 0 and dist[nx][ny] == -1:
+                queue.append((nx, ny))
+                dist[nx][ny] = dist[xp][yp] + 1
+    
+    mx = 0
+    for i in range(M):
+        for j in range(N):
+            if dist[i][j] == -1 and graph[i][j] == 0:
+                return -1
+        mx = max(mx, max(dist[i]))
+    return mx
+    
+print(func(N, M, graph))

2c84779f4f6f143ba1427723f7491dabd9dae53a
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2026-01-25 23:11:11 +0900

    [Gold V] Title: 탑, Time: 416 ms, Memory: 155260 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,19 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input().rstrip())
-nums = list(map(int, input().split()))
+N = int(input().strip())
+towers = list(map(int, input().split()))
 
-stack = []
-ans = [0] * n
+def solution(N, towers):
+    answer = [0] * N
+    stk = [] # (인덱스, 높이)
+    
+    for i, tower in enumerate(towers):
+        while stk and stk[-1][1] < tower:
+            stk.pop()
+        if stk and stk[-1][1] > tower:
+            answer[i] = stk[-1][0] + 1
+        stk.append((i, tower))
+        
+    return " ".join(map(str, answer))
 
-for i in range(n, 0, -1):
-    h = nums.pop()
-
-    while stack and stack[-1][1] < h:
-        ans[stack[-1][0] - 1] = i
-        stack.pop()
-
-    stack.append((i, h))
-
-print(*ans)
+print(solution(N, towers))

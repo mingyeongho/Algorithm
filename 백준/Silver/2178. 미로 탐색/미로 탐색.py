b7212eb594343f822e5c7036801812d1a0129150
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2026-02-09 11:49:50 +0900

    [Silver I] Title: 미로 탐색, Time: 56 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,28 +3,33 @@ from collections import deque
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-graph = [list(input().rstrip()) for _ in range(n)]
-
 direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-visited = [[False] * m for _ in range(n)]
+
+R, C = map(int, input().split())
+graph = [list(input().strip()) for _ in range(R)]  # 문자로 존재 '1', '0'
 
 
-def bfs(x, y):
-  queue = deque([(x, y)])
-  visited[x][y] = True
-  while queue:
-    xp, yp = queue.popleft()
-    for dx, dy in direction:
-      nx, ny = xp + dx, yp + dy
-      if not (0 <= nx < n and 0 <= ny < m):
-        continue
-      if graph[nx][ny] == "0" or visited[nx][ny]:
-        continue
-      queue.append((nx, ny))
-      visited[nx][ny] = True
-      graph[nx][ny] = int(graph[xp][yp]) + 1
+def bfs(R, C, graph, x, y, dist):
+    dist[x][y] = 1
+    deq = deque([(x, y)])
+
+    while deq:
+        xp, yp = deq.popleft()
+        if xp == R - 1 and yp == C - 1:
+            return dist[xp][yp]
+        for dx, dy in direction:
+            nx, ny = xp + dx, yp + dy
+            if not (0 <= nx < R and 0 <= ny < C):
+                continue
+            if graph[nx][ny] == "1" and dist[nx][ny] == -1:
+                deq.append((nx, ny))
+                dist[nx][ny] = dist[xp][yp] + 1
+    return dist[R - 1][C - 1]
 
 
-bfs(0, 0)
-print(graph[n - 1][m - 1])
+def solution(R, C, graph):
+    dist = [[-1] * C for _ in range(R)]
+    return bfs(R, C, graph, 0, 0, dist)
+
+
+print(solution(R, C, graph))

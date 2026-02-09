b38f1ae0b78a4ccfe6eb392c49a47e8129a75934
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2026-02-09 11:40:32 +0900

    [Silver I] Title: 그림, Time: 264 ms, Memory: 36544 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,39 +1,44 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
-
-visited = [[False] * M for _ in range(N)]
 direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
+R, C = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(R)]
 
-def bfs(graph, x, y, visited) -> int:
-    queue = deque([(x, y)])
+
+def bfs(R, C, graph, x, y, visited):
     visited[x][y] = True
-    area = 1
-    while queue:
-        xp, yp = queue.popleft()
+    deq = deque([(x, y)])
+    area = 1  # 그림의 넓이
+
+    while deq:
+        xp, yp = deq.popleft()
         for dx, dy in direction:
             nx, ny = xp + dx, yp + dy
-            if not (0 <= nx < N and 0 <= ny < M):
+            if not (0 <= nx < R and 0 <= ny < C):
                 continue
-            if not visited[nx][ny] and graph[nx][ny] == 1:
-                queue.append((nx, ny))
+            if graph[nx][ny] == 1 and not visited[nx][ny]:
+                deq.append((nx, ny))
                 visited[nx][ny] = True
                 area += 1
     return area
 
 
-count = 0
-mx = 0
-for i in range(N):
-    for j in range(M):
-        if not visited[i][j] and graph[i][j] == 1:
-            count += 1
-            area = bfs(graph, i, j, visited)
-            mx = max(mx, area)
+def solution(R, C, graph):
+    visited = [[False] * C for _ in range(R)]
+    cnt = 0  # 그림의 개수
+    mx_area = 0  # 가장 넓은 그림
 
-print(count)
-print(mx)
+    for i in range(R):
+        for j in range(C):
+            if graph[i][j] == 1 and not visited[i][j]:
+                area = bfs(R, C, graph, i, j, visited)
+                cnt += 1
+                mx_area = max(mx_area, area)
+    return [cnt, mx_area]
+
+
+print(*solution(R, C, graph), sep="\n")

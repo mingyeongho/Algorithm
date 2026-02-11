7a9f76474dbc87392e7a68b1b462d5f775b5c930
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2026-02-12 07:15:07 +0900

    [Gold V] Title: 토마토, Time: 1492 ms, Memory: 58592 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,37 +1,46 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
-M, N, H = map(int, input().split())
-graph = [[list(map(int, input().split())) for _ in range(N)]for _ in range(H)]
 
-dist = [[[-1] * M for _ in range(N)] for _ in range(H)]
-direction = [(-1, 0, 0), (1, 0, 0), (0, -1, 0),
-             (0, 1, 0), (0, 0, -1), (0, 0, 1)]
-queue = deque([])
-for k in range(H):
-    for i in range(N):
-        for j in range(M):
-            if dist[k][i][j] == -1 and graph[k][i][j] == 1:
-                queue.append((k, i, j))
-                dist[k][i][j] = 0
+direction = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)]
 
-while queue:
-    zp, xp, yp = queue.popleft()
-    for dz, dx, dy in direction:
-        nz, nx, ny = zp + dz, xp + dx, yp + dy
-        if not (0 <= nz < H and 0 <= nx < N and 0 <= ny < M):
-            continue
-        if dist[nz][nx][ny] == -1 and graph[nz][nx][ny] == 0:
-            queue.append((nz, nx, ny))
-            dist[nz][nx][ny] = dist[zp][xp][yp] + 1
 
-mx = 0
-for k in range(H):
-    for i in range(N):
-        for j in range(M):
-            if dist[k][i][j] == -1 and graph[k][i][j] == 0:
-                print(-1)
-                exit(0)
-            mx = max(mx, dist[k][i][j])
-print(mx)
+def solution(R, C, H, graph):
+    tomatoes = deque([])
+    dist = [[[-1] * C for _ in range(R)] for _ in range(H)]
+
+    for k in range(H):
+        for i in range(R):
+            for j in range(C):
+                if graph[k][i][j] == 1:
+                    tomatoes.append((k, i, j))
+                    dist[k][i][j] = 0
+                elif graph[k][i][j] == -1:
+                    dist[k][i][j] = 0
+
+    while tomatoes:
+        zp, xp, yp = tomatoes.popleft()
+        for dz, dx, dy in direction:
+            nz, nx, ny = zp + dz, xp + dx, yp + dy
+            if not (0 <= nz < H and 0 <= nx < R and 0 <= ny < C):
+                continue
+            if dist[nz][nx][ny] == -1:
+                dist[nz][nx][ny] = dist[zp][xp][yp] + 1
+                tomatoes.append((nz, nx, ny))
+
+    mx = -1
+    for k in range(H):
+        for i in range(R):
+            for j in range(C):
+                if dist[k][i][j] == -1:
+                    return -1
+                mx = max(mx, dist[k][i][j])
+    return mx
+
+
+M, N, H = map(int, input().split())  # M: 가로, N: 세로, H: 높이
+graph = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
+
+print(solution(N, M, H, graph))

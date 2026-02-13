8273bf4d36eb6b270a289112335c1dd5d3da8d54
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2026-02-13 10:57:43 +0900

    [Silver I] Title: 나이트의 이동, Time: 968 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -3,34 +3,31 @@ from collections import deque
 
 input = sys.stdin.readline
 
-n = int(input().rstrip())
-
-direction = [(-2, 1), (-1, 2), (1, 2), (2, 1),
-             (2, -1), (1, -2), (-1, -2), (-2, -1)]
+direction = [(-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]
 
 
-def bfs(x, y, end_x, end_y, dist, l):
-    queue = deque([(x, y)])
-    dist[x][y] = 0
-    while queue:
-        xp, yp = queue.popleft()
-        if xp == end_x and yp == end_y:
+def solution(L, st_x, st_y, ed_x, ed_y, dist):
+    dist[st_x][st_y] = 0
+    deq = deque([(st_x, st_y)])
+
+    while deq:
+        xp, yp = deq.popleft()
+        if xp == ed_x and yp == ed_y:
             return dist[xp][yp]
         for dx, dy in direction:
             nx, ny = xp + dx, yp + dy
-            if not (0 <= nx < l and 0 <= ny < l):
+            if not (0 <= nx < L and 0 <= ny < L):
                 continue
-            if dist[nx][ny] != -1:
-                continue
-            queue.append((nx, ny))
-            dist[nx][ny] = dist[xp][yp] + 1
-    return 0
+            if dist[nx][ny] == -1:
+                deq.append((nx, ny))
+                dist[nx][ny] = dist[xp][yp] + 1
+    return -1
 
 
-for _ in range(n):
-    l = int(input().rstrip())
-    start_x, start_y = map(int, input().split())
-    end_x, end_y = map(int, input().split())
-
-    dist = [[-1] * l for _ in range(l)]
-    print(bfs(start_x, start_y, end_x, end_y, dist, l))
+T = int(input().strip())
+for _ in range(T):
+    L = int(input().strip())
+    st_x, st_y = map(int, input().split())
+    ed_x, ed_y = map(int, input().split())
+    dist = [[-1] * L for _ in range(L)]
+    print(solution(L, st_x, st_y, ed_x, ed_y, dist))

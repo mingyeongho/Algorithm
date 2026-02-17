4b904ccdd479c450667ed6982cfa65daf30bc2b6
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2026-02-17 22:34:43 +0900

    [Silver I] Title: 안전 영역, Time: 512 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -3,37 +3,42 @@ from collections import deque
 
 input = sys.stdin.readline
 
-n = int(input().rstrip())
-map = [list(map(int, input().split())) for _ in range(n)]
+N = int(input().strip())
+graph = [list(map(int, input().split())) for _ in range(N)]
 
 direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-mx = 0
 
 
-def bfs(map, x, y, visited, rain):
-    queue = deque([(x, y)])
+def BFS(H, N, graph, x, y, visited):
     visited[x][y] = True
-    while queue:
-        xp, yp = queue.popleft()
+    deq = deque([(x, y)])
+
+    while deq:
+        xp, yp = deq.popleft()
         for dx, dy in direction:
             nx, ny = xp + dx, yp + dy
-            if not (0 <= nx < n and 0 <= ny < n):
+            if not (0 <= nx < N and 0 <= ny < N):
                 continue
-            if visited[nx][ny]:
-                continue
-            if map[nx][ny] > rain:
-                queue.append((nx, ny))
+            if graph[nx][ny] > H and not visited[nx][ny]:
+                deq.append((nx, ny))
                 visited[nx][ny] = True
 
 
-for rain in range(101):
-    visited = [[False] * n for _ in range(n)]
-    count = 0
-    for i in range(n):
-        for j in range(n):
-            if map[i][j] > rain and not visited[i][j]:
-                bfs(map, i, j, visited, rain)
-                count += 1
-    mx = max(mx, count)
+def solution(N, graph):
+    mx = -1
+    for k in range(101):  # 비의 높이
+        visited = [[False] * N for _ in range(N)]
+        area = 0
 
-print(mx)
+        for i in range(N):
+            for j in range(N):
+                if graph[i][j] > k and not visited[i][j]:
+                    BFS(k, N, graph, i, j, visited)
+                    area += 1
+        if area == 0:
+            break
+        mx = max(mx, area)
+    return mx
+
+
+print(solution(N, graph))

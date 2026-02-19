f3fea45682371e32f69896ea9f463b127ec2965e
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2026-02-19 13:06:36 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2056 ms, Memory: 118244 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -3,27 +3,29 @@ from collections import deque
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-graph = [list(map(int, input().strip())) for _ in range(N)]
+graph = [list(input().strip()) for _ in range(N)]
 
 direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-dist = [[[-1] * M for _ in range(N)] for _ in range(2)]  # z, x, y
-queue = deque([(0, 0, 0)])  # z, x, y
-dist[0][0][0] = 1
-while queue:
-    zp, xp, yp = queue.popleft()
-    if xp == N-1 and yp == M-1:
-        print(dist[zp][xp][yp])
-        exit(0)
-    for dx, dy in direction:
-        nx, ny = xp + dx, yp + dy
-        if not (0 <= nx < N and 0 <= ny < M):
-            continue
-        # 이동할 곳이 벽이면서 내가 벽을 부술 수 있으면
-        if graph[nx][ny] == 1 and zp == 0:
-            queue.append((1, nx, ny))
-            dist[1][nx][ny] = dist[zp][xp][yp] + 1
-        # 이동할 곳이 이동할 수 있으면서 아직 방문하지 않았다면
-        if graph[nx][ny] == 0 and dist[zp][nx][ny] == -1:
-            queue.append((zp, nx, ny))
-            dist[zp][nx][ny] = dist[zp][xp][yp] + 1
-print(-1)
+
+def solution(R, C, graph):
+    dist = [[[-1] * C for _ in range(R)] for _ in range(2)]
+    deq = deque([(0, 0, 0)]) # z, x, y
+    dist[0][0][0] = 1
+    
+    while deq:
+        zp, xp, yp = deq.popleft()
+        if xp == R-1 and yp == C-1:
+            return dist[zp][xp][yp]
+        for dx, dy in direction:
+            nx, ny = xp + dx, yp + dy
+            if not (0 <= nx < R and 0 <= ny < C):
+                continue
+            if graph[nx][ny] == "0" and dist[zp][nx][ny] == -1: # 다음 칸이 방문한 적 없는 이동할 수 있는 칸일 경우
+                deq.append((zp, nx, ny))
+                dist[zp][nx][ny] = dist[zp][xp][yp] + 1
+            elif graph[nx][ny] == "1" and zp == 0 and dist[1][nx][ny] == -1: # 벽을 부술 수 있고 다음 칸이 벽일 경우
+                deq.append((1, nx, ny))
+                dist[1][nx][ny] = dist[0][xp][yp] + 1
+    return -1
+
+print(solution(N, M, graph))

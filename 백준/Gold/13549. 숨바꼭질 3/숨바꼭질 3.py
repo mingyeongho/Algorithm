7acdce1fd8af22e2e78821ddcc04b64e0281c2fb
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2026-02-23 08:55:48 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 104 ms, Memory: 38164 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,23 +1,30 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
-n, k = map(int, input().split())
+N, K = map(int, input().split())
 
-dist = [-1] * 100_001
-queue = deque([n])
-dist[n] = 0
-while queue:
-    nx = queue.popleft()
-    if nx == k:
-        print(dist[nx])
-        break
-    for idx, calc in enumerate([nx*2, nx-1, nx+1]):
-        if not (0 <= calc < 100_001):
-            continue
-        if dist[calc] == -1:
-            if idx == 0:
-                dist[calc] = dist[nx]
-            else:
-                dist[calc] = dist[nx] + 1
-            queue.append(calc)
+
+def solution(N, K):
+    dist = [-1] * 100_001
+    q = deque([N])
+    dist[N] = 0
+
+    while q:
+        xp = q.popleft()
+        if xp == K:
+            return dist[xp]
+        for i, nx in enumerate([xp * 2, xp - 1, xp + 1]):
+            if not (0 <= nx < 100_001):
+                continue
+            if i == 0 and dist[nx] == -1:
+                dist[nx] = dist[xp]
+                q.append(nx)
+            elif dist[nx] == -1:
+                dist[nx] = dist[xp] + 1
+                q.append(nx)
+    return -1
+
+
+print(solution(N, K))

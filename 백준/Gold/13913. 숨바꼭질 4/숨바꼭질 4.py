68ee67097fad09b2adb08312dac22e05afc64fe4
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2026-02-25 08:47:02 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 160 ms, Memory: 42064 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -1,35 +1,39 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
 
-dist = [-1] * 100_001
-path = [-1] * 100_001
 
-queue = deque([N])
-dist[N] = 0
-path[N] = 0
+def solution(N, K):
+    dist = [-1] * 100_001  # 시간이 들어있는 배열
+    direct = [-1] * 100_001  # 이전 경로가 들어있는 배열
 
-while queue:
-    xp = queue.popleft()
-    if xp == K:
-        break
-    for nx in [xp*2, xp-1, xp+1]:
-        if not (0 <= nx < 100_001):
-            continue
-        if dist[nx] == -1:
-            queue.append((nx))
-            dist[nx] = dist[xp] + 1
-            path[nx] = xp
+    q = deque([N])
+    dist[N] = 0
 
-print(dist[K])
+    while q:
+        xp = q.popleft()
+        if xp == K:
+            d = []
+            end = xp
+            while direct[end] != -1:
+                d.append(end)
+                end = direct[end]
+            d.append(end)
+            d.reverse()
+            return (dist[xp], d)
+        for nx in [xp * 2, xp + 1, xp - 1]:
+            if not (0 <= nx < 100_001):
+                continue
+            # 다음 좌표의 이전 경로가 없고 시간도 없을 경우
+            if dist[nx] == -1 and direct[nx] == -1:
+                dist[nx] = dist[xp] + 1
+                direct[nx] = xp
+                q.append(nx)
 
-P = []
-while K != N:
-    P.append(K)
-    K = path[K]
-P.append(N)
-P.reverse()
 
-print(*P)
+dist, direct = solution(N, K)
+print(dist)
+print(*direct)

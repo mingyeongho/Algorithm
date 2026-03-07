4c58ee9da9cbfcdb3f7cc63bbf0bcae1c492fe11
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2026-03-07 12:02:24 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 304 ms, Memory: 69400 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,30 +1,26 @@
 import sys
-from collections import deque
+
+sys.setrecursionlimit(10**9)
+
 input = sys.stdin.readline
 
 N = int(input().strip())
-tree = [[] for _ in range(N+1)]
-for _ in range(N-1):
-    A, B = map(int, input().split())
-    tree[A].append(B)
-    tree[B].append(A)
+tree = [[] for _ in range(N + 1)]
+for _ in range(N - 1):
+    a, b = map(int, input().split())
+    tree[a].append(b)
+    tree[b].append(a)
+
+parents = [0, 1] + [-1] * (N - 1)
 
 
-parents = [0, 1] + [-1] * (N-1)
+def dfs(start, tree, parents):
+    for nx in tree[start]:
+        if parents[nx] == -1:
+            parents[nx] = start
+            dfs(nx, tree, parents)
 
 
-def bfs(node):
-    queue = deque([node])
-    while queue:
-        xp = queue.popleft()
-        for nx in tree[xp]:
-            if parents[xp] == nx:
-                continue
-            queue.append(nx)
-            parents[nx] = xp
+dfs(1, tree, parents)
 
-
-bfs(1)
-
-for i in range(2, N+1):
-    print(parents[i])
+print(*parents[2:], sep="\n")

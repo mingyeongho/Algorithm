cc1e7fa8425632a565f69e71169beab81fe4b503
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2026-03-17 09:57:04 +0900

    [Gold V] Title: 치킨 배달, Time: 268 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,41 +1,46 @@
 import sys
+
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-graph = [[0] * (N+1)] + [[0] + list(map(int, input().split()))
-                         for _ in range(N)]
+graph = [list(map(int, input().split())) for _ in range(N)]
 
-H = []
-C = []
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        if graph[i][j] == 2:
-            C.append((i, j))
-        elif graph[i][j] == 1:
-            H.append((i, j))
+homes = []  # (i, j, dist)
+chickens = []  # (i, j, flag)
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 1:
+            homes.append([i, j, float("Inf")])
+        elif graph[i][j] == 2:
+            chickens.append([i, j, False])
 
-answer = []
-dist = float("Inf")
+mn = float("Inf")
+selected = [None] * M
 
 
-def backtrack(start, k):
-    global dist
-
+def backtrack(k, start):
+    global mn
     if k == M:
-        d = 0
-        for hx, hy in H:
-            hd = float("Inf")
-            for cx, cy in answer:
-                cd = abs(hx - cx) + abs(hy - cy)
-                hd = min(hd, cd)
-            d += hd
-        dist = min(dist, d)
+        for home in homes:
+            dist = [-1] * len(selected)
+            for idx, chicken in enumerate(selected):
+                d = abs(chicken[0] - home[0]) + abs(chicken[1] - home[1])
+                dist[idx] = d
+            home[2] = min(dist)
+        acc = 0
+        for home in homes:
+            acc += home[2]
+        mn = min(mn, acc)
         return
-    for i in range(start, len(C)):
-        answer.append(C[i])
-        backtrack(i+1, k+1)
-        answer.pop()
+    for idx in range(start, len(chickens)):
+        chicken = chickens[idx]
+        if not chicken[2]:
+            chicken[2] = True
+            selected[k] = chicken
+            backtrack(k + 1, idx + 1)
+            chicken[2] = False
 
 
 backtrack(0, 0)
-print(dist)
+
+print(mn)

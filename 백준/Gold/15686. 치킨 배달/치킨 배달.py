49d8114afae940daf1b25d0ae48f45103ed89c55
Min GyeongHo <57670160+mingyeongho@users.noreply.github.com>
2026-03-17 10:05:16 +0900

    [Gold V] Title: 치킨 배달, Time: 212 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -5,14 +5,14 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
 
-homes = []  # (i, j, dist)
-chickens = []  # (i, j, flag)
+homes = []  # (i, j)
+chickens = []  # (i, j)
 for i in range(N):
     for j in range(N):
         if graph[i][j] == 1:
-            homes.append([i, j, float("Inf")])
+            homes.append((i, j))
         elif graph[i][j] == 2:
-            chickens.append([i, j, False])
+            chickens.append((i, j))
 
 mn = float("Inf")
 selected = [None] * M
@@ -21,24 +21,18 @@ selected = [None] * M
 def backtrack(k, start):
     global mn
     if k == M:
-        for home in homes:
-            dist = [-1] * len(selected)
-            for idx, chicken in enumerate(selected):
-                d = abs(chicken[0] - home[0]) + abs(chicken[1] - home[1])
-                dist[idx] = d
-            home[2] = min(dist)
         acc = 0
-        for home in homes:
-            acc += home[2]
+        for hx, hy in homes:
+            dist = float("Inf")
+            for cx, cy in selected:
+                d = abs(cx - hx) + abs(cy - hy)
+                dist = min(dist, d)
+            acc += dist
         mn = min(mn, acc)
         return
     for idx in range(start, len(chickens)):
-        chicken = chickens[idx]
-        if not chicken[2]:
-            chicken[2] = True
-            selected[k] = chicken
-            backtrack(k + 1, idx + 1)
-            chicken[2] = False
+        selected[k] = chickens[idx]
+        backtrack(k + 1, idx + 1)
 
 
 backtrack(0, 0)
